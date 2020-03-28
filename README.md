b1f4b719f6e10bb215a90c6c466be51f0e3125c8
EdvardsZ <edvards1999@gmail.com>
2020-03-28 09:11:12 +0200

    Read commandline arguments

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Linux sistēmu programmēšana: MD5
 
 Uzdevums: http://andromeda.df.lu.lv/wiki/index.php/LU-LSP-b:MD5
+
+Compile & Run:
+
+gcc main.c BestFit/bestfit.c FirstFit/firstfit.c NextFit/nextfit.c WorstFit/worstfit.c -o projectMD5; ./projectMD5 -c chunks -s size
--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include "BestFit/bestfit.h"
 #include "FirstFit/firstfit.h"
 #include "NextFit/nextfit.h"
 #include "WorstFit/worstfit.h"
 
-int main()
-{
-    printf("hello world\n");
+int main(int argc, char *argv[]) {
+    int option;
+    char chunksFile[256];
+    char sizesFile[256];
+    while((option = getopt(argc, argv, "c:s:")) != -1){ //get option from the getopt() method
+        switch(option){
+            case 'c': 
+                strcpy(chunksFile,optarg);
+                printf("Given File: %s\n", optarg);
+                break;
+            case 's':
+                strcpy(sizesFile,optarg);
+                printf("Given File: %s\n", optarg);
+                break;   
+        }
+    }
+    //-----Nolasīti komandrindas argumenti
 }
